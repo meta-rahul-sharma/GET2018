e2208a590d23414fb7b9964a30aaea1f7646c7f1
meta-rahul-sharma <rahul.sharma3@metacube.com>
2018-08-18 21:52:49 +0530

    dbms session 3 assignment

---
+++ DBMS/DBMS-Session-3_Assignment-2.sql
@@ -0,0 +1,51 @@
+USE storefront;
+/*
+Display the list of products (Id, Title, Count of Categories) which fall in more than one Categories.
+*/
+SELECT products.product_id, products.product_name, COUNT(categorylink.category_id) count_of_categories
+FROM products INNER JOIN categorylink ON
+    products.product_id = categorylink.product_id
+GROUP BY categorylink.product_id
+HAVING COUNT(categorylink.category_id) > 1;
+/*
++------------+----------------+---------------------+
+| product_id | product_name   | count_of_categories |
++------------+----------------+---------------------+
+|          1 | Redmi 3s Prime |                   2 |
+|          2 | Redmi Note 4   |                   2 |
++------------+----------------+---------------------+
+*/
+
+/*
+Display Count of products as per below price range 0-1000,1001-5000,Above 5000
+*/
+SELECT price_range, count(*) no_of_products
+FROM (SELECT CASE
+                WHEN price >= 0 AND price <= 1000 THEN '0-1000'
+                WHEN price >= 1001 AND price <= 5000 THEN '1001-5000'
+                ELSE 'Above 5000'
+            END as price_range
+      FROM products) as count_of_products
+GROUP BY price_range;
+/*
++-------------+----------------+
+| price_range | no_of_products |
++-------------+----------------+
+| Above 5000  |              6 |
+| 0-1000      |              1 |
+| 1001-5000   |              5 |
++-------------+----------------+
+*/
+
+/*
+Display the Categories along with number of products under each category.
+*/
+SELECT * 
+FROM categories;
+
+
+SELECT parent.category_id, parent.category_name, COUNT(link.product_id)
+FROM categories child , categories parent, categorylink link
+WHERE (parent.category_id = child.parent_id OR parent.category_id = child.category_id)
+AND child.category_id = link.category_id
+GROUP BY parent.category_id;
