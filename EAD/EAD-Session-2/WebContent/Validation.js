eb2f02b2cf8ed599f64e2ad05aa8eff9cc3d2bfb
rahul sharma <rahul.sharma3@metacube.com>
2018-08-29 16:36:11 +0530

    EAD-Session-2-Assignments

--- EAD/EAD-Session-2/WebContent/Validation.js
+++ EAD/EAD-Session-2/WebContent/Validation.js
@@ -4,11 +4,16 @@
 function validateName(nameId) {
 	var result = true;
 	var name = document.getElementById(nameId).value;
+	var alphabetCondition = /^([a-zA-Z]*)$/
 	document.getElementById(nameId).style.border = "2px solid green";
+	document.getElementById("firstNameMessanger").innerHTML = "";
 	if (name.length < 3) {
-		alert(name + " length should be greater than 2");
+		document.getElementById("firstNameMessanger").innerHTML = "Length should be greater than 2";
 		document.getElementById(nameId).style.border = "2px solid red";
 		result = false;
+	} else if(!alphabetCondition.test(name)) {
+		document.getElementById("firstNameMessanger").innerHTML = "Should Only contain Alphabet's";
+		document.getElementById(nameId).style.border = "2px solid red";
 	}
 	return result;
 }
@@ -20,11 +25,12 @@ function validateForm() {
 function validateEmail(emailId) {
 	var result = true;
 	var email = document.getElementById(emailId).value;
+	document.getElementById("emailMessanger").innerHTML = "";
 	var atpos = email.indexOf("@");
 	var dotpos = email.lastIndexOf(".");
 	document.getElementById(emailId).style.border = "2px solid green";
 	if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length || email.length < 7) {
-		alert("Not a valid e-mail address");
+		document.getElementById("emailMessanger").innerHTML = "Not a valid e-mail address";
 		document.getElementById(emailId).style.border = "2px solid red";
 		result = false;
 	}
@@ -34,17 +40,18 @@ function validateEmail(emailId) {
 function validatePassword(passwordId) {
 	var password = document.getElementById(passwordId).value;
 	var result = false;
+	document.getElementById("passwordMessanger").innerHTML = "";
 	document.getElementById(passwordId).style.border = "2px solid red";
 	if (password.length < 8) {
-	    alert("Your password must be at least 8 characters")
+	    document.getElementById("passwordMessanger").innerHTML = "Your password must be at least 8 characters";
 	} else if (password.search(/[a-z]/) < 0) {
-		alert("Your password must contain at least one lowercase letter.");
+		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one lowercase letter.";
 	} else if (password.search(/[A-Z]/) < 0) {
-		alert("Your password must contain at least one uppercase letter.");
+		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one uppercase letter.";
 	} else if (password.search(/[0-9]/) < 0) {
-		alert("Your password must contain at least one digit.");
+		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one digit.";
 	} else if(password.search(/[\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:\-]/) < 0) {
-		alert("Your password must contain at least one special character.");
+		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one special character.";
 	} else {
 		document.getElementById(passwordId).style.border = "2px solid green";
 		result = true;
@@ -56,11 +63,12 @@ function validateContact(contactId) {
 	var contact = document.getElementById(contactId).value;
 	var contactCondition = /^([0-9]*)$/
 	var result = false;
+	document.getElementById("contactMessanger").innerHTML = "";
 	document.getElementById(contactId).style.border = "2px solid red";
 	if(!contactCondition.test(contact)) {
-		alert("Your contact must contain digit only");
+		document.getElementById("contactMessanger").innerHTML = "Your contact must contain digit only";
 	} else if(contact.length < 9) {
-		alert("Your contact must contain at least 8 numbers");
+		document.getElementById("contactMessanger").innerHTML = "Your contact must contain at least 8 numbers";
 	} else {
 		document.getElementById(contactId).style.border = "2px solid green";
 		result = true;
@@ -71,9 +79,10 @@ function validateContact(contactId) {
 function validateConfirmPassword(confirmPasswordId) {
 	var confirmPassword = document.getElementById(confirmPasswordId).value;
 	var result = true;
+	document.getElementById("confirmMessanger").innerHTML = "";
 	document.getElementById(confirmPasswordId).style.border = "2px solid red";
 	if(confirmPassword == document.getElementById("password").value) {
-		alert("Password Do not Matches");
+		document.getElementById("confirmMessanger").innerHTML = "Password Do not Matches";
 		document.getElementById(confirmPasswordId).style.border = "2px solid green";
 		result = false;
 	}
