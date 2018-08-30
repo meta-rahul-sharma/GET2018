ab33eeef214f489286636e2b36cd5083b0638c50
meta-rahul-sharma <rahul.sharma3@metacube.com>
2018-08-31 01:09:46 +0530

    changes

--- EAD/EAD-Session-2/WebContent/Validation.js
+++ EAD/EAD-Session-2/WebContent/Validation.js
@@ -1,18 +1,18 @@
 /**
- * Used for validation of sign up form
+ * Used for validation of sign up form and login form
  */
-function validateName(nameId) {
+function validateName(nameId, spanId) {
 	var result = true;
 	var name = document.getElementById(nameId).value;
 	var alphabetCondition = /^([a-zA-Z]*)$/
 	document.getElementById(nameId).style.border = "2px solid green";
-	document.getElementById("firstNameMessanger").innerHTML = "";
+	document.getElementById(spanId).innerHTML = "";
 	if (name.length < 3) {
-		document.getElementById("firstNameMessanger").innerHTML = "Length should be greater than 2";
+		document.getElementById(spanId).innerHTML = "Length should be greater than 2";
 		document.getElementById(nameId).style.border = "2px solid red";
 		result = false;
 	} else if(!alphabetCondition.test(name)) {
-		document.getElementById("firstNameMessanger").innerHTML = "Should Only contain Alphabet's";
+		document.getElementById(spanId).innerHTML = "Should Only contain Alphabet's";
 		document.getElementById(nameId).style.border = "2px solid red";
 	}
 	return result;
@@ -22,51 +22,56 @@ function validateForm() {
 
 }
 
-function validateEmail(emailId) {
+function validateEmail(emailId, spanId) {
 	var result = true;
 	var email = document.getElementById(emailId).value;
-	document.getElementById("emailMessanger").innerHTML = "";
+	document.getElementById(spanId).innerHTML = "";
 	var atpos = email.indexOf("@");
 	var dotpos = email.lastIndexOf(".");
 	document.getElementById(emailId).style.border = "2px solid green";
 	if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length || email.length < 7) {
-		document.getElementById("emailMessanger").innerHTML = "Not a valid e-mail address";
+		document.getElementById(spanId).innerHTML = "Not a valid e-mail address";
 		document.getElementById(emailId).style.border = "2px solid red";
 		result = false;
 	}
 	return result;
 }
 
-function validatePassword(passwordId) {
+function validatePassword(passwordId, spanId) {
 	var password = document.getElementById(passwordId).value;
 	var result = false;
-	document.getElementById("passwordMessanger").innerHTML = "";
+	document.getElementById(spanId).innerHTML = "";
 	document.getElementById(passwordId).style.border = "2px solid red";
 	if (password.length < 8) {
-	    document.getElementById("passwordMessanger").innerHTML = "Your password must be at least 8 characters";
+	    document.getElementById(spanId).innerHTML = "Your password must be at least 8 characters";
 	} else if (password.search(/[a-z]/) < 0) {
-		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one lowercase letter.";
+		document.getElementById(spanId).innerHTML = "Your password must contain at least one lowercase letter.";
 	} else if (password.search(/[A-Z]/) < 0) {
-		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one uppercase letter.";
+		document.getElementById(spanId).innerHTML = "Your password must contain at least one uppercase letter.";
 	} else if (password.search(/[0-9]/) < 0) {
-		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one digit.";
+		document.getElementById(spanId).innerHTML = "Your password must contain at least one digit.";
 	} else if(password.search(/[\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:\-]/) < 0) {
-		document.getElementById("passwordMessanger").innerHTML = "Your password must contain at least one special character.";
+		document.getElementById(spanId).innerHTML = "Your password must contain at least one special character.";
 	} else {
 		document.getElementById(passwordId).style.border = "2px solid green";
 		result = true;
 	}
+	
+	if(document.getElementById("confirmPassword").value != "") {
+		validateConfirmPassword("confirmPassword", "confirmSpan");
+	}
+	
 	return result;
 }
 
-function validateContact(contactId) {
+function validateContact(contactId, spanId) {
 	var contact = document.getElementById(contactId).value;
 	var contactCondition = /^([0-9]*)$/
 	var result = false;
-	document.getElementById("contactMessanger").innerHTML = "";
+	document.getElementById(spanId).innerHTML = "";
 	document.getElementById(contactId).style.border = "2px solid red";
 	if(!contactCondition.test(contact)) {
-		document.getElementById("contactMessanger").innerHTML = "Your contact must contain digit only";
+		document.getElementById(spanId).innerHTML = "Your contact must contain digit only";
 	} else if(contact.length < 9) {
 		document.getElementById("contactMessanger").innerHTML = "Your contact must contain at least 8 numbers";
 	} else {
@@ -76,13 +81,13 @@ function validateContact(contactId) {
 	return true;
 }
 
-function validateConfirmPassword(confirmPasswordId) {
+function validateConfirmPassword(confirmPasswordId, spanId) {
 	var confirmPassword = document.getElementById(confirmPasswordId).value;
 	var result = true;
-	document.getElementById("confirmMessanger").innerHTML = "";
+	document.getElementById(spanId).innerHTML = "";
 	document.getElementById(confirmPasswordId).style.border = "2px solid green";
 	if(confirmPassword != document.getElementById("password").value) {
-		document.getElementById("confirmMessanger").innerHTML = "Password Do not Matches";
+		document.getElementById(spanId).innerHTML = "Password Do not Matches";
 		document.getElementById(confirmPasswordId).style.border = "2px solid red";
 		result = false;
 	}
@@ -91,8 +96,8 @@ function validateConfirmPassword(confirmPasswordId) {
 
 function validateSignUpForm() {
 	var result = false;
-	if(validateName("firstName") && validateName("lastName") && validateEmail("email") &&
-			validatePassword("password") && validateConfirmPassword("confirmPassword") && validateContact("contact")) {
+	if(validateName("firstName", "firstSpan") && validateName("lastName", "lastSpan") && validateEmail("email", "emailSpan") &&
+			validatePassword("password", "passwordSpan") && validateConfirmPassword("confirmPassword", "confirmSpan") && validateContact("contact", "contactSpan")) {
 		result = true;
 	}
 	return result;
@@ -101,7 +106,7 @@ function validateSignUpForm() {
 function validateLoginForm() {
 	var result = false;
 	
-	if(validateEmail("loginEmail") && validatePassword("loginPassword")) {
+	if(validateEmail("loginEmail", "loginEmailSpan") && validatePassword("loginPassword", "loginPasswordSpan")) {
 		result = true;
 	}
 	return result;
