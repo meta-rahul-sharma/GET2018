b678e6392df8bb438a569401108c353fdabb5bbf
rahul sharma <rahul.sharma3@metacube.com>
2018-09-03 17:20:58 +0530

    EAD-Session4-Assignment

--- EAD/EAd-Session-4&5/src/com/servlets/ProfileServlet.java
+++ EAD/EAd-Session-4&5/src/com/servlets/ProfileServlet.java
@@ -44,13 +44,18 @@ public class ProfileServlet extends HttpServlet {
 			out.print("<body><table width = '100%' class = 'header'>");
 			out.print("<tr><td><img src='html.png' id='logo' height = 50px></td>");
 			out.print("<td align = 'right'><span style = 'color : white' id = 'username'>"
-					+ user.getFirstName() + " " + user.getLastName() + "  |  "
-					+ "</span><a href = 'FriendServlet?email=" + user.getEmail() + "'>Friends</a><span style = 'color : white'> | </span><a href = 'LogoutServlet'>Logout</a></td>");
+					+ user.getFirstName()
+					+ " "
+					+ user.getLastName()
+					+ "  |  "
+					+ "</span><a href = 'FriendServlet?email="
+					+ user.getEmail()
+					+ "'>Friends</a><span style = 'color : white'> | </span><a href = 'LogoutServlet'>Logout</a></td>");
 			out.print("</tr></table>");
 			out.print("<table width = '100%'><tr><td>");
 			out.print("<table width = '50%' align = 'center'><tr>");
-			out.print("<tr><td align = 'center'><h2>User Details</h2><br/><br/><td></tr>");
-			out.print("<td><p><strong>First Name:</strong></p></td>");
+			out.print("<td align = 'center'><h2>User Details</h2><br/><br/></td></tr>");
+			out.print("<tr><td><p><strong>First Name:</strong></p></td>");
 			out.print("<td><p>" + user.getFirstName() + "</p></td></tr>");
 			out.print("<tr><td><p><strong>Last Name:</strong></p></td>");
 			out.print("<td><p>" + user.getLastName() + "</p></td></tr>");
@@ -62,27 +67,29 @@ public class ProfileServlet extends HttpServlet {
 			out.print("<td><p>" + user.getContact() + "</p></td></tr>");
 			out.print("<tr><td><p><strong>Organization:</strong></p></td>");
 			out.print("<td><p>" + user.getOrganization() + "</p></td></tr>");
-			out.print("<tr><td><form action = 'Update' method = 'post'><input type='submit' value='Edit Details'>" + 
-					"<input type='hidden' name='hiddenEmail' value= '" + email + "'/></form></td></tr></table></td>");
+			out.print("<tr><td><form action = 'Update' method = 'post'><input type='submit' value='Edit Details'>"
+					+ "<input type='hidden' name='hiddenEmail' value= '"
+					+ email + "'/></form></td></tr></table></td>");
 			out.print("<td><table width = '50%' align = 'center'><tr><td>");
 			out.print("<h1>Welcome</h1>");
 			out.print("</td></tr>");
 			out.println("<tr><td><form action = 'SetProfileImageServlet' method='post' enctype = 'multipart/form-data'>"
-				+ "<input type='hidden' name='hiddenEmail' value='" + email + "'/>");
+					+ "<input type='hidden' name='hiddenEmail' value='"
+					+ email
+					+ "'/>");
 			if (profileImage.exists()) {
-				out.print("<img src='" + profileImage + "' height = '30%'/><br>");
+				out.print("<img src='" + profileImage
+						+ "' height = '30%'/><br>");
 			} else {
-				out.print("<img src='" + filePath + "\\signup.png' height = '30%'/><br>");
+				out.print("<img src='C:\\Users\\User3\\workspace\\EAd-Session-4\\src\\com\\Images\\signup.jpg' height = '30%'/><br>");
 			}
-			out.println(
-					"<input type = 'file' name = 'file' size = '20' /><br /><input type = 'submit' value = 'Upload File' /></form></td></tr>");
-			out.println("<tr><td>"+ user.getFirstName() +"</td></tr>");
-			out.print("</table></body></html>");
-			
+			out.println("<input type = 'file' name = 'file' size = '20' /><br /><input type = 'submit' value = 'Upload File' /></form></td></tr>");
+			out.println("<tr><td>" + user.getFirstName() + "</td></tr>");
+			out.print("</table></td></tr></table></body></html>");
+
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 		out.close();
 	}
-
 }
