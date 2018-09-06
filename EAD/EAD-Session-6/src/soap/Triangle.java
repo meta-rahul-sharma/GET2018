a3d7e8fa8f67aa664ed7f95e732f1049b52c227b
rahul sharma <rahul.sharma3@metacube.com>
2018-09-06 16:00:46 +0530

    commenting

--- EAD/EAD-Session-6/src/soap/Triangle.java
+++ EAD/EAD-Session-6/src/soap/Triangle.java
@@ -2,15 +2,19 @@ package soap;
 
 /**
  * To get area of traingle by sides using heron's formula
- * @author Rahul Sharma
- * Creation Date: 03/ 09/ 2018
+ * 
+ * @author Rahul Sharma Creation Date: 03/ 09/ 2018
  *
  */
 public class Triangle {
 	public double area(double a, double b, double c) {
-		double halfPerimeter = ( a + b + c ) /2;
-		double area = Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
-		return area;
+		if ((a + b > c) && (b + c > a) && (c + a > b)) {
+			double halfPerimeter = (a + b + c) / 2;
+			double area = Math.sqrt(halfPerimeter * (halfPerimeter - a)
+					* (halfPerimeter - b) * (halfPerimeter - c));
+			return area;
+		} else {
+			return -1;
+		}
 	}
 }
-
