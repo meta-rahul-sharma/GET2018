5331f4f35a3e46787d5ff3538514289d9d6512e2
rahul sharma <rahul.sharma3@metacube.com>
2018-10-04 18:09:10 +0530

    UI-Session-7

--- UI/UI-7/dom.js
+++ UI/UI-7/dom.js
@@ -3,8 +3,8 @@ function removeIfChildExist() {
     var childCount = form.childElementCount;
     
     if(childCount > 9) {
-        form.removeChild(form.children[8]);
-        form.removeChild(form.children[8]);
+        form.removeChild(form.children[7]);
+        form.removeChild(form.children[7]);
     }
     
 }
@@ -17,13 +17,14 @@ function selectedState() {
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
     
     if(selectedValue === "rajasthan") {
-        addWebSiteName();
         addDescriptor();
+        addWebSiteName();
     } else if(selectedValue === "haryana") {
         addZipCode();
-        /*addHosting();*/
+        addHosting();
     } else if(selectedValue === "maharastra") {
-        
+        addDescriptor();
+        addZipCode();
     }
 }
 
@@ -54,7 +55,7 @@ function addWebSiteName() {
     z.appendChild(icon);
     z.appendChild(urlBox);
     z.appendChild(span);
-    document.getElementById('form').appendChild(x);
+    document.getElementById('form').insertBefore(x, document.getElementById('form').children[7]);
 }
 
 function addDescriptor() {
@@ -72,13 +73,14 @@ function addDescriptor() {
     var z = document.createElement("div"),
     icon = document.createElement("i"),
     descriptionBox = document.createElement("textarea");
+    descriptionBox.setAttribute("id", "description");
     icon.setAttribute("class", "fa fa-pencil singUpIcon");
     
     z.setAttribute("class", "signUpIconAndContent");
     x.appendChild(z);
     z.appendChild(icon);
     z.appendChild(descriptionBox);
-    document.getElementById('form').appendChild(x);
+    document.getElementById('form').insertBefore(x, document.getElementById('form').children[7]);
 }
 
 function addZipCode() {
@@ -108,7 +110,7 @@ function addZipCode() {
     z.appendChild(icon);
     z.appendChild(zipBox);
     z.appendChild(span);
-    document.getElementById('form').appendChild(x);
+    document.getElementById('form').insertBefore(x, document.getElementById('form').children[7]);
 }
 
 function addHosting() {
@@ -124,18 +126,140 @@ function addHosting() {
     y.appendChild(label);
 
     var z = document.createElement("div"),
-    icon = document.createElement("i"),
-    urlBox = document.createElement("input"),
-    span = document.createElement("span");
-    icon.setAttribute("class", "fa fa-globe singUpIcon");
-    urlBox.setAttribute("type", "text");
-    urlBox.setAttribute("id", "hosting");
-    span.setAttribute("id", "urlSpan");
-    urlBox.setAttribute("onblur", "isUrl(id, 'urlSpan')");
+    radioYes = document.createElement("input"),
+    radioNo = document.createElement("input"),
+    spanYes = document.createElement("span"),
+    spanNo =  document.createElement("span"),
+    breakElement = document.createElement("br");
+    radioYes.setAttribute("type", "radio");
+    radioNo.setAttribute("type", "radio");
+    radioYes.setAttribute("id", "hosting");
+    radioNo.setAttribute("id", "hosting");
+    radioYes.setAttribute("name", "hosting");
+    radioNo.setAttribute("name", "hosting");
+    spanYes.textContent = "Yes";
+    spanNo.textContent = "No";
     z.setAttribute("class", "signUpIconAndContent");
     x.appendChild(z);
-    z.appendChild(icon);
-    z.appendChild(urlBox);
-    z.appendChild(span);
-    document.getElementById('form').appendChild(x);
+    z.appendChild(radioYes);
+    z.appendChild(spanYes);
+    z.appendChild(breakElement);
+    z.appendChild(radioNo);
+    z.appendChild(spanNo);
+    document.getElementById('form').insertBefore(x, document.getElementById('form').children[7]);
+}
+
+function validateSignUpForm() {
+	var result = true,
+    errorMessage = "",
+    removalbalChildFirst = document.getElementById('form').children[7],
+    removalbalChildSecond = document.getElementById('form').children[8];
+    
+	if(!validateName("firstName", "firstNameSpan")) {
+        errorMessage = "firstName : Name Details Invalid\n";
+        result = false;
+    }
+    if(!validateName("lastName", "lastNameSpan") ) {
+        errorMessage = errorMessage + "lastName : Name Details Invalid\n";
+        result = false;
+    }
+    if(!validateEmail("email", "emailSpan")) {
+        errorMessage = errorMessage + "email : Email Details are Incorrect\n";  
+        result = false;
+    }
+    if(!validateName("city", "citySpan") ) {
+        errorMessage = errorMessage + "city : city Details Invalid\n";
+        result = false;
+    }
+  /*  if(!isUrl("domainName", "urlSpan")) {
+        errorMessage = errorMessage + "url : url Details Incorrect\n";
+    }*/
+    
+    if(result) {
+        alert("Successfully Submitted");
+        storeFormOnLocal();
+    } else {
+        alert(errorMessage);    
+    }
+    
+	return result;
+}
+
+function storeFormOnLocal() {
+    var firstName = document.getElementById("firstName");
+    localStorage.setItem("firstName", firstName.value);
+    
+    var lastName = document.getElementById("lastName");
+    localStorage.setItem("lastName", lastName.value);
+    
+    var email = document.getElementById("email");
+    localStorage.setItem("email", email.value);
+    
+    var phoneNumber = document.getElementById("phoneNumber");
+    localStorage.setItem("phoneNumber", phoneNumber.value);
+    
+    var address = document.getElementById("address");
+    localStorage.setItem("address", address.value);
+    
+    var city = document.getElementById("city");
+    localStorage.setItem("city", city.value);
+    
+    var state = document.getElementById("state");
+    localStorage.setItem("state", state.value);
+    
+    if(state.value === "rajasthan") {
+        var domainName = document.getElementById("domainName");
+    localStorage.setItem("domainName", domainName.value);
+        
+        var description = document.getElementById("description");
+    localStorage.setItem("description", description.value);
+        
+    } else if(state.value === "haryana") {
+        var zipcode = document.getElementById("zipcode");
+    localStorage.setItem("zipcode", zipcode.value);
+        
+        var hosting = document.getElementById("hosting");
+    localStorage.setItem("hosting", hosting.value);
+        
+    } else if(state.value === "maharastra") {
+        var zipcode = document.getElementById("zipcode");
+    localStorage.setItem("zipcode", zipcode.value);
+        
+        var hosting = document.getElementById("hosting");
+    localStorage.setItem("hosting", hosting.value);
+    
+    }
+}
+
+function fetchLocalStroageDetails() {
+    debugger;
+    var archive = [],
+        keys = Object.keys(localStorage),
+        i = 0, key;
+
+    for (; key = keys[i]; i++) {
+        archive.push( key + '=' + localStorage.getItem(key));
+        
+        if(localStorage.getItem(key) != null && localStorage.getItem(key) != "") {
+        
+            var x = document.createElement("div"),
+             y = document.createElement("div");
+            x.setAttribute("class", "signUpFormRow");
+            y.setAttribute("class", "signUpTag");
+            var label = document.createElement("p");
+            label.textContent = key;
+            x.appendChild(y);
+            y.appendChild(label);
+
+            var z = document.createElement("div"),
+            zipBox = document.createElement("p");
+            zipBox.textContent = localStorage.getItem(key);
+            z.setAttribute("class", "signUpIconAndContent");
+            x.appendChild(z);
+            z.appendChild(zipBox);
+            document.getElementById('formDetails').appendChild(x);
+        }
+    }
+
+    return archive;
 }
--- UI/UI-7/script.js
+++ UI/UI-7/script.js
@@ -49,6 +49,7 @@ function isUrl(urlId, spanId)
     document.getElementById(spanId).innerHTML = "";
     document.getElementById(urlId).style.border = "2px solid green";
     regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    
     if (!regexp.test(urlValue))
     {   
         document.getElementById(spanId).innerHTML = "Url is Incorrect";
@@ -56,14 +57,6 @@ function isUrl(urlId, spanId)
         isUrl = false;
     }
     return isUrl;
-}
-
-function validateSignUpForm() {
-	var result = false;
-	if(validateName("firstName", "firstSpan") && validateName("lastName", "lastSpan") && validateEmail("email", "emailSpan") && validateContact("contact", "contactSpan")) {
-		result = true;
-	}
-	return result;
 }
 
 function isNumber(evt) {
