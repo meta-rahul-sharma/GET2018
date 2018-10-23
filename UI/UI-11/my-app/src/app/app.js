b822f5c96933bc83035cbf483c5e5384d274cb7e
rahul sharma <rahul.sharma3@metacube.com>
2018-10-23 16:09:22 +0530

    changes

--- UI/UI-11/my-app/src/app/app.js
+++ UI/UI-11/my-app/src/app/app.js
@@ -1,4 +1,6 @@
-var myApp = angular.module("app", ["ngRoute"]);
+var myApp = angular.module("app", ["ngRoute", 'cartDetail', 'shoppingDetail', 'orderDone']);
+
+
 myApp.config(['$locationProvider' ,'$routeProvider',
     function config($locationProvider, $routeProvider) {
         $locationProvider.hashPrefix('!');
@@ -8,14 +10,17 @@ myApp.config(['$locationProvider' ,'$routeProvider',
             controller: "itemController"
         })
         .when("/shopping-cart", {
-            templateUrl : "src/pages/cart.html"
+            template: '<cart-detail></cart-detail>'
         })
-        .when("/check-out", {
-            templateUrl : "src/pages/checkout.html"
+        .when("/shopping-address", {
+            template: '<shopping-detail></shopping-detail>'
         })
         .when("/category/:categoryName", {
             templateUrl : "src/pages/home.html",
             controller: "itemController"
+        })
+        .when("/orders", {
+            template: '<order-done></order-done>'
         })
         .otherwise({
             redirectTo: '/'   
@@ -26,11 +31,54 @@ myApp.controller("itemController", function ($scope, $http, $rootScope, $routePa
 
     $scope.categoryName = $routeParams.categoryName;
     console.log($scope.categoryName);
-    /**
+    /** 
      * Updates the userlist in the view
      */
     $http.get("http://localhost:3000/data").then(function (response) {
         $scope.items = response.data;
-    }
-)});
+    });
+    
+    this.cartCount = 0;
+    var self = this;
+    $http.get('http://localhost:3000/shopping-cart').then(function (response) {
+        self.cartItems = response.data;
+        var item;
+        for (item in self.cartItems) {
+            self.cartCount += self.cartItems[item].count;
+        }
+        $rootScope.totalCartCount = self.cartCount;
+    });
+                                                 
+    $scope.addCart = function(item) {
+        $http.get('http://localhost:3000/shopping-cart/' + item.id).then(function successCallback(response){
+            response.data.count += 1;
+            console.log(response.data.count);
+            response.data.price = response.data.count * item.price;
+            console.log(response.data.price);
+            $http({
+                method: 'PUT',
+                url:'http://localhost:3000/shopping-cart/' + item.id,
+                data: response.data,
+                dataType:'json'
+            });
+            window.location.reload();
+        },function errorCallback(response){
 
+            var cartData = {
+                "id":item.id,
+                "item":item.item,
+                "price":item.price,
+                "imagePath":item.imagePath,
+                "count":1
+            }
+
+            $http({
+                method: 'POST',
+                url:'http://localhost:3000/shopping-cart/',
+                data: cartData,
+                dataType:'json'
+            })
+            window.location.reload();
+        });
+    }
+});
